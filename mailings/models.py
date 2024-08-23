2337519c3901ade4538d0a9061792b2dde7b35dd
dddendyy <dbokasin@yandex.ru>
2024-08-23 22:07:20 +0300

    Первый коммит. Создан проект и приложение, добавлен шаблон домашней страницы.

--- mailings/models.py
+++ mailings/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=150, verbose_name='ФИО')
+    email = models.EmailField()
+    comment = models.TextField(verbose_name='комментарий')
+
+
+class Mailing(models.Model):
+
+    PERIODICITY_CHOICES = [
+        ('daily', 'Ежедневно'),
+        ('weekly', 'Еженедельно'),
+        ('monthly', 'Ежемесячно')
+    ]
+
+    STATUS_CHOICES = [
+        ('created', 'Создана'),
+        ('launched', 'Запущена'),
+        ('finished', 'Завершена')
+    ]
+
+    first_send_datetime = models.DateTimeField(verbose_name='дата/время первой отправки')
+    periodicity = models.CharField(max_length=30, choices=PERIODICITY_CHOICES)
+    status = models.CharField(max_length=30, choices=STATUS_CHOICES)
+
